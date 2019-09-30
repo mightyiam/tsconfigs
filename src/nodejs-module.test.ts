37e06292275e204bb647973eca6fae99ce98864b
Shahar Or (mightyiam) <mightyiampresence@gmail.com>
2019-09-30 21:02:03 +0700

    feat: complete rewrite
    BREAKING CHANGE: Complete rewrite.
    
    Co-authored-by: Cleaver Barnes <cbarnes@cleaver.ca>

---
+++ src/nodejs-module.test.ts
@@ -0,0 +1,54 @@
+import test from 'ava'
+import { unitTest } from './unit-test-macro'
+import { common, importable } from './shared'
+import { integrationTest } from './integration-test-macro'
+
+test(unitTest, 'nodejs-module', {
+  compilerOptions: {
+    ...common,
+    ...importable,
+    lib: ['ESNext'],
+    module: 'CommonJS'
+  }
+})
+
+test(integrationTest, {
+  name: 'nodejs-module',
+  additionalDependencies: ['@types/node'],
+  srcFiles: [
+    {
+      filename: 'index.ts',
+      contents: [
+        'process.exit()',
+        'export const foo = 0'
+      ].join('\n')
+    }
+  ],
+  expectedOutputFiles: [
+    {
+      filename: 'index.js',
+      expectedContents: [
+        '"use strict";',
+        'Object.defineProperty(exports, "__esModule", { value: true });',
+        'process.exit();',
+        'exports.foo = 0;',
+        '//# sourceMappingURL=index.js.map'
+      ].join('\n')
+    },
+    {
+      filename: 'index.d.ts',
+      expectedContents: 'export declare const foo = 0;\n'
+    },
+    {
+      filename: 'index.js.map',
+      expectedContents: JSON.stringify({
+        version: 3,
+        file: 'index.js',
+        sourceRoot: '',
+        sources: ['../src/index.ts'],
+        names: [],
+        mappings: ';;AAAA,OAAO,CAAC,IAAI,EAAE,CAAA;AACD,QAAA,GAAG,GAAG,CAAC,CAAA'
+      })
+    }
+  ]
+})
---
+++ src/unit-test-macro.ts
@@ -0,0 +1,62 @@
+import { Macro, ExecutionContext } from 'ava'
+import { resolve, dirname } from 'path'
+import { readFileSync } from 'fs'
+import { libMap, CompilerOptions, ModuleKind, ModuleResolutionKind, NewLineKind, ScriptTarget, readConfigFile, parseJsonConfigFileContent, sys } from 'typescript'
+import { ConfigName } from './shared'
+declare module 'typescript' {
+  export const libMap: Map<string>
+}
+
+type OptionValue = CompilerOptions[string]
+type OptionParser = (value: any) => CompilerOptions[string]
+
+const optionParsers: { [key: string ]: OptionParser } = {
+  lib: (libs: string[]): string[] => libs.map((lib): string => libMap.get(lib.toLowerCase()) as string),
+  module: (kind: ModuleKind): string => ModuleKind[kind],
+  moduleResolution: (kind: string): string => {
+    const map: any = {
+      node: ModuleResolutionKind.NodeJs,
+      classic: ModuleResolutionKind.Classic
+    }
+    return map[kind.toLowerCase()]
+  },
+  newLine: (kind: string): string => {
+    const map: any = {
+      lf: NewLineKind.LineFeed,
+      crlf: NewLineKind.CarriageReturnLineFeed
+    }
+    return map[kind.toLowerCase()]
+  },
+  target: (kind: ScriptTarget): string => ScriptTarget[kind]
+}
+
+const rawToCompilerOptions = (raw: any): CompilerOptions => ({
+  configFilePath: undefined,
+  ...Object.fromEntries(
+    Object.entries(raw)
+      .map(([key, value]: [string, any]): [string, OptionValue] => {
+        const parser = optionParsers[key]
+        if (parser === undefined) return [key, value]
+        return [key, parser(value)]
+      })
+  )
+})
+
+export const unitTest: Macro<[ConfigName, any]> = (
+  t: ExecutionContext,
+  configName: ConfigName,
+  { compilerOptions: rawExpectedOptions }: any
+): void => {
+  const expectedOptions = rawToCompilerOptions(rawExpectedOptions)
+
+  const path = resolve(__dirname, '..', `${configName}.json`)
+  const { config } = readConfigFile(
+    path,
+    (path: string): string => readFileSync(path, { encoding: 'utf-8' })
+  )
+  const actual = parseJsonConfigFileContent(config, sys, dirname(path))
+
+  t.deepEqual(actual.options, expectedOptions)
+}
+
+unitTest.title = () => 'unit'
